e637fc073ebd500e729129fe6a8d990c8656988c
Yvon TRAN <tran.yvon@gmail.com>
2013-12-03 15:53:18 +0100

    base de données

--- Web Services/smartblog.sql
+++ Web Services/smartblog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Sam 23 Novembre 2013 à 14:38
+-- Généré le: Mar 03 Décembre 2013 à 14:53
 -- Version du serveur: 5.6.12-log
 -- Version de PHP: 5.4.12
 
@@ -25,6 +25,41 @@ USE `smartblog`;
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `articles`
+--
+
+CREATE TABLE IF NOT EXISTS `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `contenu` text NOT NULL,
+  `nb_vues` int(11) NOT NULL DEFAULT '0',
+  `like` int(11) NOT NULL DEFAULT '0',
+  `dislike` int(11) NOT NULL DEFAULT '0',
+  `id_auteur` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaires`
+--
+
+CREATE TABLE IF NOT EXISTS `commentaires` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `contenu` text NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `id_auteur` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `utilisateurs`
 --
 
@@ -36,8 +71,17 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `password` varchar(255) NOT NULL,
   `date_tentative` datetime NOT NULL,
   `nb_tentatives` int(11) NOT NULL,
+  `last_article` datetime DEFAULT NULL,
+  `last_commentaire` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `login`, `password`, `date_tentative`, `nb_tentatives`, `last_article`, `last_commentaire`) VALUES
+(1, 'Super', 'Admin', 'test@test.fr', 'efd302a1f125f0b107975d2f1f87ce0ae59beaadff', '2013-12-03 13:27:31', 0, NULL, NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
